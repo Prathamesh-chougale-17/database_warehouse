4d26ae2feb0f900a8fcf01d2432c2c677aebbc85
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-08-22 21:20:50 +0530

    saving first work

---
+++ classwork/demo.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the data
+print(5)
+df = pd.read_csv('D:\study\python\database_warehouse\classwork\demo.csv')
